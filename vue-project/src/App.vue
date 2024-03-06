2084fcb6942992e43acfb11598af5300aae01348
Itbrandon <brandoniticka@gmail.com>
2024-03-06 15:39:44 -0800

    vue-with-tailwind

---
+++ vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <h2 class="text-blue-500 text-4xl font-bold">Hello</h2>
+</template>
+
+<style>
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
